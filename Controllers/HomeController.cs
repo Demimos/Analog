ea782be4dd5988eaaddb117d73a74fad173116b2
Demimos <43846185+Demimos@users.noreply.github.com>
2020-01-21 23:41:26 +0300

    Добавьте файлы проекта.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Analog.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(string Zones)
+        {
+            var selected = (Zones == null) ? TimeZoneInfo.Local.Id : HttpUtility.HtmlDecode(Zones);
+            ViewData["current"] = selected;
+            ViewBag.Zones = TimeZoneInfo.GetSystemTimeZones().Select(z=>new SelectListItem() { 
+                Value=z.Id,
+                Text=z.DisplayName,
+                Selected=z.Id.Equals(selected)
+            });
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Controllers/TimeService.asmx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace Analog.Controllers
+{
+    /// <summary>
+    /// Сводное описание для TimeService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Чтобы разрешить вызывать веб-службу из скрипта с помощью ASP.NET AJAX, раскомментируйте следующую строку. 
+     [System.Web.Script.Services.ScriptService]
+    public class TimeService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public int[] GetTime(string zone)
+        {
+            var offset = TimeZoneInfo.GetSystemTimeZones().First(z => z.Id == zone).BaseUtcOffset;
+            var d = DateTime.UtcNow.AddHours(offset.Hours).AddMinutes(offset.Minutes);
+            return new int[] { d.Hour, d.Minute, d.Second };
+        }
+    }
+}
